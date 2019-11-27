0cd7349998db6698782a40ef9b612dcfd91659fd
Sebastian Crossa <crossasebastian@gmail.com>
2019-11-26 20:45:32 -0600

    UI para la vista de usuarios creada

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,14 +1,33 @@
 // Libraries
-import React from "react";
+import React, { useState } from "react";
+import useInputState from "../hooks/useInputState";
 
 // Component imports
+import Input from "../layout/components/Input";
 import { Navbar } from "../layout/components/Navbar";
+import { Header, InputCard, SearchButton } from "./users.style";
+import { UserList } from "../containers/Feed/UserList";
 
 const Users = () => {
+  const [buscar, updateBuscar, resetBuscar] = useInputState("");
+
   return (
     <div>
       <Navbar />
-      <h1 style={{ marginTop: "5rem" }}>User list</h1>
+
+      <div style={{ margin: "0 auto 2rem auto" }}>
+        <Header>Search for other users</Header>
+        <InputCard>
+          <Input
+            type="text"
+            value={buscar}
+            onChange={updateBuscar}
+            placeholder="Buscar..."
+          />
+          <SearchButton>BUSCAR</SearchButton>
+        </InputCard>
+      </div>
+      <UserList />
     </div>
   );
 };
